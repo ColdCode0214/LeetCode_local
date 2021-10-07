84ee9a79776492de865e71483a18f90cb3a402ca
ColdCode0214 <jwang21@wpi.edu>
2021-10-07 15:50:02 -0400

    003.前n个数字二进制中1的个数

---
+++ 003.前n个数字二进制中1的个数.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        ans.push_back(0);
+        for(int i=1;i<=n;i++)
+        {
+            if(i%2==0)
+                ans.push_back(ans[i/2]);
+            else
+                ans.push_back(ans[i-1]+1);
+        }
+        return ans;
+    }
+};
