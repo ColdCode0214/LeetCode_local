5ed7e2e91663b7cc8e7634295112b5ff869743e9
ColdCode0214 <jwang21@wpi.edu>
2021-10-23 22:22:19 -0400

    231-2的幂

---
+++ 231-2的幂.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0 || (n%2==1 && n!=1))
+            return false;
+        while(n>2)
+        {
+            if(n%2==1)
+                return false;
+            n/=2;
+        }
+        return true;
+    }
+};
