f779aceabdec77c1429eac48873f1272b7ce101f
ColdCode0214 <jwang21@wpi.edu>
2021-10-27 22:20:20 -0400

    278-第一个错误的版本

---
+++ 278-第一个错误的版本.cpp
@@ -0,0 +1,18 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int l=1,r=n;
+        while(l<=r)
+        {
+            int mid=(r-l)/2+l;
+            if(isBadVersion(mid)==true)
+                r=mid-1;
+            else
+                l=mid+1;
+        }
+        return max(l,r);
+    }
+};
