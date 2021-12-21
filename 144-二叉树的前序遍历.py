add4a7c9c1e18da48c79a0ab9008027fbb544042
ColdCode0214 <jwang21@wpi.edu>
2021-12-20 23:53:20 -0500

    144-二叉树的前序遍历

---
+++ 144-二叉树的前序遍历.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root == None:
+            return []
+        ans = []
+        ans.append(root.val)
+        ans.extend(self.preorderTraversal(root.left))
+        ans.extend(self.preorderTraversal(root.right))
+        return ans
