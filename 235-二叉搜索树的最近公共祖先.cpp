508d58e0fa1047cc3155d3509873880f411b3a67
ColdCode0214 <jwang21@wpi.edu>
2021-12-21 21:06:19 -0500

    235-二叉搜索树的最近公共祖先

---
+++ 235-二叉搜索树的最近公共祖先.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while(true)
+        {
+            if(root->val > max(p->val, q->val))
+                root=root->left;
+            else if(root->val < min(p->val, q->val))
+                root=root->right;
+            else
+                break;
+        }
+        return root;
+    }
+};
