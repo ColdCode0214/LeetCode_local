84bc5a3256d3014575108dc71f303bfcbad0f0b9
ColdCode0214 <jwang21@wpi.edu>
2021-12-29 16:19:05 -0500

    056-二叉搜索树中两个节点之和

---
+++ 056-二叉搜索树中两个节点之和.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root, vector<int>& ans)
+    {
+        if(!root)
+            return;
+        inorder(root->left, ans);
+        ans.push_back(root->val);
+        inorder(root->right, ans);
+    }
+    bool findTarget(TreeNode* root, int k) {
+        vector<int> ans;
+        inorder(root, ans);
+        int left=0, right=ans.size()-1;
+        while(left<right)
+        {
+            if(ans[left]+ans[right]==k)
+                return true;
+            else if(ans[left]+ans[right]>k)
+                right--;
+            else
+                left++;
+        }
+        return false;
+    }
+};
---
+++ 056-二叉搜索树中两个节点之和.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTarget(self, root: TreeNode, k: int) -> bool:
+        def inorder(root, ans):
+            if not root:
+                return
+            inorder(root.left, ans)
+            ans.append(root.val)
+            inorder(root.right, ans)
+        ans = []
+        inorder(root, ans)
+        left = 0
+        right = len(ans)-1
+        while(left<right):
+            if ans[left] + ans[right] == k:
+                return True
+            elif ans[left] + ans[right] > k:
+                right-=1
+            else:
+                left+=1
+        return False
