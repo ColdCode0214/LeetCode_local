6364899b0592092763dbabc959ad99c2ba0d8a62
ColdCode0214 <jwang21@wpi.edu>
2021-12-29 16:18:35 -0500

    0404-检查平衡性

---
+++ 0404-检查平衡性.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        def height(root) -> int:
+            if root == None:
+                return 0
+            leftHeight = height(root.left)
+            rightHeight = height(root.right)
+            if leftHeight == -1 or rightHeight == -1 or abs(leftHeight - rightHeight)>1:
+                return -1
+            else:
+                return max(leftHeight, rightHeight)+1
+        return height(root) >=0
