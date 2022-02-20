6b7145736730ffcbc10ca06827374bee8c7e3827
ColdCode0214 <jwang21@wpi.edu>
2022-02-19 23:01:30 -0500

    1791-找出星型图的中心节点

---
+++ 1791-找出星型图的中心节点.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        if(edges[1][0]==edges[0][0] || edges[1][0]==edges[0][1])
+            return edges[1][0];
+        else
+            return edges[1][1];
+    }
+};
