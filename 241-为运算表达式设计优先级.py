5c04e0b6971579dec3d7dd3f8530af6355caf461
ColdCode0214 <jwang21@wpi.edu>
2022-05-12 23:34:00 -0400

    241-为运算表达式设计优先级

---
+++ 241-为运算表达式设计优先级.py
@@ -0,0 +1,18 @@
+class Solution:
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+        if expression.isdigit():
+            return [int(expression)]
+        res = []
+        for i, char in enumerate(expression):
+            if char in ['+', '-', '*']:
+                left = self.diffWaysToCompute(expression[:i])
+                right = self.diffWaysToCompute(expression[i+1:])
+                for l in left:
+                    for r in right:
+                        if char == '+':
+                            res.append(l+r)
+                        elif char == '-':
+                            res.append(l-r)
+                        else:
+                            res.append(l*r)
+        return res
