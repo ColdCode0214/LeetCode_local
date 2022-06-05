4627558aacd667cfa57659499f488b3d723ac5f3
ColdCode0214 <jwang21@wpi.edu>
2022-06-05 01:24:53 -0400

    1342-将数字变成0的操作次数

---
+++ 1342-将数字变成0的操作次数.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        count = 0
+        while num != 0:
+            if num%2 == 1:
+                num -= 1
+            else:
+                num /= 2
+            count += 1
+        return count
