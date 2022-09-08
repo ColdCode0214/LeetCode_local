9b78dd45efe7868486ad0a48f50a2198c4c0a879
ColdCode0214 <jwang21@wpi.edu>
2022-09-08 16:05:10 -0400

    1013-将数组分成和相等的三个部分

---
+++ 1013-将数组分成和相等的三个部分 【快】.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canThreePartsEqualSum(self, arr: List[int]) -> bool:
+        if sum(arr)%3 != 0: return False
+        tar = int(sum(arr)/3)
+        # print("tar:", tar)
+        count, sum_ = 0, 0
+        n = len(arr)
+        for i in range(n):
+            sum_ += arr[i]
+            if sum_ == tar:
+                count += 1
+                sum_ = 0
+            if count == 2:
+                if i != n-1:
+                    return True
+                else:
+                    return False
+        return False
